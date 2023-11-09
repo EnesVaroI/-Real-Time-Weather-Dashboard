a77ba1c1fda530abcb3c19c5cfb300ad3251888e
enesvarol189 <104737357+enesvarol189@users.noreply.github.com>
2023-11-09 10:36:41 +0100

    Add files via upload

---
+++ ClimaDash/Controllers/WeatherController.cs
@@ -0,0 +1,67 @@
+﻿using ClimaDash.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClimaDash.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        //private readonly IWeatherService _weatherService;
+
+        //public WeatherController(IWeatherService weatherService)
+        //{
+        //    _weatherService = weatherService;
+        //}
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetWeatherData(string city)
+        //{
+        //    try
+        //    {
+        //        //WeatherData weatherData
+        //        var weatherData = await _weatherService.GetWeatherAsync(city);
+        //        return Ok(weatherData);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+
+        //-------------------------------------------------------------------------------------//
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetWeatherData(double lat, double lon)
+        //{
+        //    try
+        //    {
+        //        //WeatherData weatherData
+        //        var weatherData = await _weatherService.GetWeatherDataAsync(lat, lon);
+        //        return Ok(weatherData);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+
+        //private readonly IHubContext<WeatherHub> _hubContext;
+
+        //public WeatherController(IHubContext<WeatherHub> hubContext)
+        //{
+        //    _hubContext = hubContext;
+        //}
+
+        //public async Task<string> GetWeatherData(string cityName)
+        //{
+        //    var weatherData = await WeatherService.GetWeatherData(cityName);
+
+        //    var weatherDataJson = JsonSerializer.Serialize(weatherData);
+        //    return weatherDataJson;
+        //}
+    }
+}
---
+++ ClimaDash/Controllers/WeatherHub.cs
@@ -0,0 +1,13 @@
+﻿using ClimaDash.Views.Components;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ClimaDash.Controllers
+{
+    public class WeatherHub : Hub
+    {
+        public async Task SendWeatherUpdate(Weather weatherData)
+        {
+            await Clients.All.SendAsync("ReceiveWeatherUpdate", weatherData);
+        }
+    }
+}
